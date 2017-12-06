da8da8fab4bfb8cab5b45729334acb25640fe5a6
遼太郎 金 <150568@jc-21.jp>
2017-12-06 22:19:59 +0900

    stack

--- SQLite/SQLite/MainPage.xaml.cs
+++ SQLite/SQLite/MainPage.xaml.cs
@@ -1,4 +1,55 @@
-﻿/*using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using ZXing.Mobile;
+using ZXing.Net.Mobile.Forms;
+
+namespace SQLite
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ScanButtonClicked(object sender, EventArgs s)
+        {
+            var scanPage = new ZXingScannerPage()
+            {
+                DefaultOverlayTopText = "バーコードを読み取ります",
+                DefaultOverlayBottomText = "",
+            };
+
+            // スキャナページを表示
+            await Navigation.PushAsync(scanPage);
+
+            scanPage.OnScanResult += (result) =>
+            {
+                // スキャン停止
+                scanPage.IsScanning = false;
+
+                // PopAsyncで元のページに戻り、結果をダイアログで表示
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    await DisplayAlert("スキャン完了", result.Text, "OK");
+                });
+
+                //scanedData.Add(result.Text);
+            };
+        }
+    }
+}
+
+/*using System;
 using Xamarin.Forms;
 
 //参考url http://dev-suesan.hatenablog.com/entry/2017/03/06/005206
@@ -152,54 +203,3 @@ namespace SQLite
     }
 }
 */
-
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-
-using ZXing.Mobile;
-using ZXing.Net.Mobile.Forms;
-
-namespace SQLite
-{
-
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        async void ScanButtonClicked(object sender, EventArgs s)
-        {
-            var scanPage = new ZXingScannerPage()
-            {
-                DefaultOverlayTopText = "バーコードを読み取ります",
-                DefaultOverlayBottomText = "",
-            };
-
-            // スキャナページを表示
-            await Navigation.PushAsync(scanPage);
-
-            scanPage.OnScanResult += (result) =>
-            {
-                // スキャン停止
-                scanPage.IsScanning = false;
-
-                // PopAsyncで元のページに戻り、結果をダイアログで表示
-                Device.BeginInvokeOnMainThread(async () =>
-                {
-                    await Navigation.PopAsync();
-                    await DisplayAlert("スキャン完了", result.Text, "OK");
-                });
-
-                //scanedData.Add(result.Text);
-            };
-        }
-    }
-}
